76759b1e8d19a6ec1215b9c9c8766c686dedb54e
borgchrist1 <christian.borg@outlook.com>
2017-09-21 14:47:06 +0200

    fixing some stuff

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+//use Weidner\Goutte\GoutteFacade;
 
 class HomeController extends Controller
 {
-    //
+    public function Index()
+    {
+        $resturants = [];
+        $current;
+        $type;
+        $dish;
+        $crawler = \Goutte::request('GET', 'https://www.lindholmen.se/pa-omradet/dagens-lunch');
+        $crawler->filter('.title a, .dish-name, .icon-dish, .table-list__column--price')->each(function ($node) use(&$resturants, &$current, &$type, &$dish) {
+           
+            if($node->attr('href')){  
+                $resturant = ['name' => $node->text(), 'menu' => []];
+                $current = array_push($resturants, $resturant) - 1;
+            } else if($node->attr('class') === 'dish-name'){
+                $dish = $node->text();  
+            } else if(strpos($node->text(), 'kr')){
+                $price = $node->text();
+                $arr = ['type' => $type, 'dish' => $dish, 'price' => $price];
+                //$resturants[$current]['menu_option'] = [];
+                array_push($resturants[$current]['menu'], $arr);
+            } else {
+                $type = $node->text();
+            }
+         });
+        return view ('welcome', ['resturants' => $resturants]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,30 +14,5 @@
 // Rou
 
 
-Route::get('/', function() {
-    
-    $resturants = [];
-    $current;
-    $type;
-    $dish;
-    $crawler = Goutte::request('GET', 'https://www.lindholmen.se/pa-omradet/dagens-lunch');
-    $crawler->filter('.title a, .dish-name, .icon-dish, .table-list__column--price')->each(function ($node) use(&$resturants, &$current, &$type, &$dish) {
-       
-        if($node->attr('href')){  
-            $resturant = ['name' => $node->text(), 'menu' => []];
-            $current = array_push($resturants, $resturant) - 1;
-        } else if($node->attr('class') === 'dish-name'){
-            $dish = $node->text();  
-        } else if(strpos($node->text(), 'kr')){
-            $price = $node->text();
-            $arr = ['type' => $type, 'dish' => $dish, 'price' => $price];
-            //$resturants[$current]['menu_option'] = [];
-            array_push($resturants[$current]['menu'], $arr);
-        } else {
-            $type = $node->text();
-        }
-     });
 
-    return response()->json($resturants)->header('Access-Controll-Allow-Origin', '*');
-});
-
+Route::get('/', 'HomeController@index');
